91db7ab852ec91df745bc74582e24d34ca19b26c
PTumilowicz <pawel.tumilowicz.pp@gmail.com>
2022-07-28 05:32:08 +0200

    Removed unsafe files

---
+++ App/Controllers/Home.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+
+class Home extends Authenticated {
+
+    public function indexAction() {
+        View::renderTemplate('Home/index.twig');
+    }
+}
