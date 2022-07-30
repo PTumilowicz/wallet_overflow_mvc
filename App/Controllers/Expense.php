e78442ab95c54a7ef94cf1fb39974349ed33edbd
PTumilowicz <pawel.tumilowicz.pp@gmail.com>
2022-07-30 05:51:08 +0200

    Adding expense category and expense payment method

--- App/Controllers/Expense.php
+++ App/Controllers/Expense.php
@@ -44,4 +44,36 @@ class Expense extends Authenticated {
         }
         $this->redirect('/expense/show');
     }
+
+    public function addCategoryAction() {
+        $expenseCategory = $_POST['new_expense_category'];
+        $user_id = $this->user->id;
+
+        $categoryToUpper = strtoupper($expenseCategory);
+
+        if (ExpenseCategory::checkIfExpenseCategoryExists($user_id, $categoryToUpper)) {
+            if (ExpenseCategory::addExpenseCategory($user_id, $expenseCategory)) {
+                $_SESSION['s_new_expense_category'] = 'New expense category added.';
+            }
+        } else {
+            $_SESSION['e_new_expense_category'] = 'Category already exists.';
+        }
+        $this->redirect('/expense/show');
+    }
+
+    public function addPaymentMethodAction() {
+        $paymentMethod = $_POST['new_payment_method'];
+        $user_id = $this->user->id;
+
+        $paymentMethodToUpper = strtoupper($paymentMethod);
+
+        if (ExpenseCategory::checkIfPaymentMethodExists($user_id, $paymentMethodToUpper)) {
+            if (ExpenseCategory::addPaymentMethod($user_id, $paymentMethod)) {
+                $_SESSION['s_new_payment_method'] = 'New payment method added.';
+            }
+        } else {
+            $_SESSION['e_new_payment_method'] = 'Payment method already exists.';
+        }
+        $this->redirect('/expense/show');
+    }
 }
--- App/Models/ExpenseCategory.php
+++ App/Models/ExpenseCategory.php
@@ -8,12 +8,6 @@ class ExpenseCategory extends \Core\Model {
 
     public $errors = [];
 
-    public function __construct($data = []) {
-        foreach ($data as $key => $value) {
-            $this->$key = $value;
-        }
-    }
-
     public static function getUserExpenseCategories($user_id) {
         $sql = 'SELECT name FROM expenses_category_assigned_to_users
                 WHERE user_id = :userId';
@@ -26,4 +20,95 @@ class ExpenseCategory extends \Core\Model {
 
         return $stmt->fetchAll();
     }
+
+    public static function checkIfExpenseCategoryExists($user_id, $category) {
+        $sql = 'SELECT name FROM (SELECT UPPER(name) AS name FROM expenses_category_assigned_to_users
+                WHERE user_id = :userId) a WHERE name = :category';
+
+        $db = static::getDB();
+
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':userId', $user_id, PDO::PARAM_INT);
+        $stmt->bindParam(':category', $category, PDO::PARAM_STR);
+
+        $stmt->execute();
+        
+        if (!$stmt->fetch(PDO::FETCH_ASSOC)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static function addExpenseCategory($user_id, $expenseCategory) {
+
+        if (static::validateCategory($expenseCategory)) {
+            $sql = 'INSERT INTO expenses_category_assigned_to_users (user_id, name)
+            VALUES (:user_id, :name)';
+    
+            $db = static::getDB();
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+            $stmt->bindParam(':name', $expenseCategory, PDO::PARAM_STR);
+    
+            return $stmt->execute();
+        }
+        return false;
+    }
+
+    public static function validateCategory($expenseCategory) {
+        $pattern = '/[^\wżźćńółęąśŻŹĆĄŚĘŁÓŃ0-9 ]/i';
+        $result = preg_match($pattern, $expenseCategory);
+
+        if ($result == 1) {
+            $_SESSION['e_new_expense_category'] = 'Only alphanumeric values and spaces allowed.';
+            return false;
+        }
+        return true;
+    }
+
+    public static function checkIfPaymentMethodExists($user_id, $payment_method) {
+        $sql = 'SELECT name FROM (SELECT UPPER(name) AS name FROM payment_methods_assigned_to_users
+                WHERE user_id = :user_id) a WHERE name = :payment_method';
+
+        $db = static::getDB();
+
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $stmt->bindParam(':payment_method', $payment_method, PDO::PARAM_STR);
+
+        $stmt->execute();
+        
+        if (!$stmt->fetch(PDO::FETCH_ASSOC)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static function addPaymentMethod($user_id, $payment_method) {
+
+        if (static::validatePaymentMethod($payment_method)) {
+            $sql = 'INSERT INTO payment_methods_assigned_to_users (user_id, name)
+                    VALUES (:user_id, :name)';
+    
+            $db = static::getDB();
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+            $stmt->bindParam(':name', $payment_method, PDO::PARAM_STR);
+    
+            return $stmt->execute();
+        }
+        return false;
+    }
+
+    public static function validatePaymentMethod($payment_method) {
+        $pattern = '/[^\wżźćńółęąśŻŹĆĄŚĘŁÓŃ0-9 ]/i';
+        $result = preg_match($pattern, $payment_method);
+
+        if ($result == 1) {
+            $_SESSION['e_new_payment_method'] = 'Only alphanumeric values and spaces allowed.';
+            return false;
+        }
+        return true;
+    }
+
 }
